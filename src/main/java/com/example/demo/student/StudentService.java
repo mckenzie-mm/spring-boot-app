146baf129b6327602c128ba7550533d26c7b5c44
mckenzie-mm <mmackenzie.uk@gmail.com>
2025-08-10 00:24:37 +1000

    JPA Repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,5 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,16 +12,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+	private final StudentRepository studentRepository;
+
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
     
     public List<Student> getStudents() {
-		return List.of(
-			new Student(
-				1L,
-				"Mariam",
-				"mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
+		return studentRepository.findAll();
 	}
 }
