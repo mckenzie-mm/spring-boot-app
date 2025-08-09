6fb4337512be0bf9f7e09fb64b30dfb2f3a9730a
mckenzie-mm <mmackenzie.uk@gmail.com>
2025-08-09 22:31:08 +1000

    Dependency Injection

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,15 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+/* 
+@Component 	// @Component creates an instance of the Student Service
+			// without this anotation the app won't work 
+			// now use @Service instead of @Component
+*/
+
+@Service
 public class StudentService {
     
     public List<Student> getStudents() {
