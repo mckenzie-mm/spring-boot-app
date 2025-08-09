7906742827e7c4dded1ee4c82656cd7aa1712a7f
mckenzie-mm <mmackenzie.uk@gmail.com>
2025-08-09 20:33:31 +1000

    Student Class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
@@ -15,8 +20,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(
+			new Student(
+				1L,
+				"Mariam",
+				"mariam.jamal@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21
+			)
+		);
 	}
 
 }
