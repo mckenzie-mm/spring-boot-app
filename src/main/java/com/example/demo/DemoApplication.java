d9b41308de2aed3d4bb2142a5a0d2508b809e5fe
mckenzie-mm <mmackenzie.uk@gmail.com>
2025-08-09 20:51:10 +1000

    API Layer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,35 +1,11 @@
 package com.example.demo;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.student.Student;
-
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-			new Student(
-				1L,
-				"Mariam",
-				"mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
 	}
 
 }
