6c15782864eb79ac1453d0a5df8840a19a295b71
mckenzie-mm <mmackenzie.uk@gmail.com>
2025-08-09 19:17:45 +1000

    return a list of data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
