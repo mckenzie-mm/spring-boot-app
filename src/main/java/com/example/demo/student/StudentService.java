88cde47486fae664fd33cdf3abcc9b315b7ea2a8
mckenzie-mm <mmackenzie.uk@gmail.com>
2025-08-10 13:19:33 +1000

    Solution

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 /* 
 @Component 	// @Component creates an instance of the Student Service
@@ -42,5 +45,29 @@ public class StudentService {
 				"student with id " + studentId + " does not exist");
 		}
 		studentRepository.deleteById(studentId);
+    }
+
+	@Transactional // entity is in managed state. If one fails, then all fails
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+			.orElseThrow(() -> new IllegalStateException(
+				"student with id " + studentId + " does not exist"
+			));
+		if (name != null && name.length() > 0 &&
+			!Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+
+		if (email != null && email.length() > 0 &&
+			!Objects.equals(student.getEmail(), email)) {
+
+			Optional<Student> studentOptional =
+				studentRepository.findStudentByEmail(student.getEmail());
+
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("email taken");
+			}
+			student.setEmail(email);
+		}
     }
 }
